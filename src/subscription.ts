106b5b0a135c1ecb1421fd341e814046378ebf6b
liuquanling <2524219830@qq.com>
2024-05-22 03:02:02 +0800

    fix: 改ID

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,12 +4,12 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
-  name: 'Subscription',
+  id: 666,
+  name: 'SSTA',
   version: 0,
   author: 'author',
   checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  supportUri: 'https://github.com/sstakill/subscription',
   categories,
   globalGroups,
   apps: await batchImportApps(`${import.meta.dirname}/apps`),
